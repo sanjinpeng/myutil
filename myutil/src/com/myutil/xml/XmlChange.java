77c4f3774dd687a79a2bc85ff4b82b2ac9ab9ea4
penggan <penggan@penggan>
2017-08-12 12:39:08 +0800

    xml转map

---
+++ myutil/src/com/myutil/xml/XmlChange.java
@@ -0,0 +1,98 @@
+package com.myutil.xml;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.Element;
+
+/**
+ * 所需jar dom4j  maven地址
+ * 	
+ * <dependency>
+	  <groupId>dom4j</groupId>
+	  <artifactId>dom4j</artifactId>
+	  <version>1.6.1</version>
+	</dependency>
+ * @author ss
+ */
+public class XmlChange {
+	public static Map<String, Object> Dom2Map(Document doc) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		if (doc == null)
+			return map;
+		Element root = doc.getRootElement();
+		for (Iterator iterator = root.elementIterator(); iterator.hasNext();) {
+			Element e = (Element) iterator.next();
+			
+			List list = e.elements();
+			if (list.size() > 0) {
+				map.put(e.getName(), Dom2Map(e));
+			} else
+				map.put(e.getName(), e.getText());
+		}
+		return map;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static Map Dom2Map(Element e) {
+		Map map = new HashMap();
+		int attributeCount = e.attributeCount();
+		if(attributeCount>0){
+			List<Attribute> attributes = e.attributes();
+			for (Attribute attribute : attributes) {
+				map.put(attribute.getName(), attribute.getValue());
+			}
+		}
+		List list = e.elements();
+		if (list.size() > 0) {
+			for (int i = 0; i < list.size(); i++) {
+				Element iter = (Element) list.get(i);
+				List mapList = new ArrayList();
+
+				if (iter.elements().size() > 0) {
+					Map m = Dom2Map(iter);
+					if (map.get(iter.getName()) != null) {
+						Object obj = map.get(iter.getName());
+						if (!obj.getClass().getName()
+								.equals("java.util.ArrayList")) {
+							mapList = new ArrayList();
+							mapList.add(obj);
+							mapList.add(m);
+						}
+						if (obj.getClass().getName()
+								.equals("java.util.ArrayList")) {
+							mapList = (List) obj;
+							mapList.add(m);
+						}
+						map.put(iter.getName(), mapList);
+					} else
+						map.put(iter.getName(), m);
+				} else {
+					if (map.get(iter.getName()) != null) {
+						Object obj = map.get(iter.getName());
+						if (!obj.getClass().getName()
+								.equals("java.util.ArrayList")) {
+							mapList = new ArrayList();
+							mapList.add(obj);
+							mapList.add(iter.getText());
+						}
+						if (obj.getClass().getName()
+								.equals("java.util.ArrayList")) {
+							mapList = (List) obj;
+							mapList.add(iter.getText());
+						}
+						map.put(iter.getName(), mapList);
+					} else
+						map.put(iter.getName(), iter.getText());
+				}
+			}
+		} else
+			map.put(e.getName(), e.getText());
+		return map;
+	}
+}
